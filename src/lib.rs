a6c6f22ceb42c6b214e77e7287a322dbe1f8e4c5
Leo Dutra <leodutra.br@gmail.com>
2020-10-25 23:45:04 -0300

    MVP

---
+++ src/lib.rs
@@ -0,0 +1,67 @@
+#[macro_use]
+extern crate lazy_static;
+
+mod countries;
+
+use countries::COUNTRIES_MAP;
+// use regex::Regex;
+
+const FLAG_MAGIC_NUMBER: u32 = 127397;
+
+// lazy_static! {
+//     static ref FLAG_RE: Regex = Regex::new(r"^[\u{0001F1E6}-\u{0001F1FF}]{2}$").unwrap();
+// }
+
+#[derive(Clone)]
+pub (crate) struct Country {
+    code: &'static str,
+    names: Vec<&'static str>,
+}
+
+impl Country {
+    pub fn name(&self) -> &'static str {
+        self.names[0]
+    }
+}
+
+pub fn is_code(code: Option<&str>) -> bool {
+    code.map_or(false, |code| COUNTRIES_MAP.contains_key(code.to_uppercase().trim()))
+}
+
+pub fn code_to_name(code: &str) -> Option<&'static str> {
+    COUNTRIES_MAP
+        .get(code.to_uppercase().trim())
+        .map(|country| country.name())
+}
+
+pub fn code_to_flag(code: &str) -> Option<String> {
+    if is_code(Some(code)) {
+        let mut flag = String::new();
+        for c in code.chars() {
+            if let Some(c) = std::char::from_u32(c as u32 + FLAG_MAGIC_NUMBER) {
+                flag.push(c);
+            } else {
+                return None;
+            }
+        }
+        Some(flag)
+    } else {
+        None
+    }
+}
+
+// pub fn is_country_flag(flag: &str) -> bool {
+//     FLAG_RE.is_match(flag)
+// }
+
+pub fn flag_to_code(flag: &str) -> Option<&'static str> {
+    let mut code = String::new();
+    for c in flag.chars() {
+        if let Some(c) = std::char::from_u32(c as u32 - FLAG_MAGIC_NUMBER) {
+            code.push(c);
+        } else {
+            return None;
+        }
+    }
+    COUNTRIES_MAP.get(code.as_str()).map(|country| country.code)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-
-#[macro_use]
-extern crate lazy_static;
-
-mod countries;
-use countries::COUNTRIES;
+use country_emoji::{code_to_flag, flag_to_code};
 
 fn main() {
-    println!("Hello, world! {}", COUNTRIES.get("BR").unwrap().get(0).unwrap());
+    // println!("Hello, world! {}", COUNTRIES_MAP.get("BR").unwrap().name());
+    println!("Hello, world! {} != {}", code_to_flag("BR").unwrap(), "BR");
+    println!("Hello, world! {} == {}", flag_to_code(code_to_flag("BR").unwrap().as_str()).unwrap(), "BR");
+    // let flag = code_to_flag(Some("AD")).unwrap();
+    // println!("flag = {}", flag);
+    // flag_to_code(&flag);
+    // let mut chars = flag.chars();
+    // println!("0x{:x} 0x{:x}", chars.next().unwrap() as u32, chars.next().unwrap() as u32)
 }
